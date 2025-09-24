4f85052afaff4be92fb5b0499e2562b2cc1b7c93
OURAJIM Amal OBS/MKT <KWQX8725@orange.com>
2025-09-24 16:06:50 +0100

    fix defining dataclass

--- src/memory_agent/context.py
+++ src/memory_agent/context.py
@@ -1,4 +1,12 @@
+"""Define the runtime context information for the agent."""
+
+import os
+from dataclasses import dataclass, field, fields
+from typing_extensions import Annotated
+
+from memory_agent import prompts
 from memory_agent.utils import GCoreChat, GCoreEmbeddings
+
 
 @dataclass(kw_only=True)
 class Context:
@@ -18,6 +26,13 @@ class Context:
     embed_api: str = field(default_factory=lambda: os.environ.get("EMBED_API", ""))
     embed_key: str = field(default_factory=lambda: os.environ.get("EMBED_KEY", ""))
     embed_model: str = field(default_factory=lambda: os.environ.get("EMBED_MODEL", ""))
+
+    def __post_init__(self):
+        for f in fields(self):
+            if not f.init:
+                continue
+            if getattr(self, f.name) == f.default:
+                setattr(self, f.name, os.environ.get(f.name.upper(), f.default))
 
     def get_chat(self):
         """Return a GCore chat model instance."""
